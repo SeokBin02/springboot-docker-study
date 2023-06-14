e951500b26b1245573f2661c80d9ee1a17fcff40
SeokBin02 <ghkdtjrqls02@gmail.com>
2023-06-14 15:44:05 +0900

    feat: project build

---
+++ src/test/java/com/example/docker_study/DockerStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
